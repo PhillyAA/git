ea070a5b0f31d46f76fdfe9da1556dd234da0b66
PhillyAA <phil.aaronovitch@gmail.com>
2024-04-11 17:08:51 +0100

    Pizza_slices_list_project

---
+++ Learn-Python/3-lists/pizza_slices.py
@@ -0,0 +1,30 @@
+# Your code below:
+
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+price = [2, 6, 1, 3, 2, 7, 2]
+
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+
+num_two_dollar_slices = price.count(2)
+
+num_pizzas = len(toppings)
+
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices.sort()
+
+cheapest_pizza = pizza_and_prices [0]
+priciest_pizza = pizza_and_prices [-1]
+
+pizza_and_prices.pop(-1)
+
+pizza_and_prices.insert(4, [2.5, "peppers"])
+#print(cheapest_pizza)
+#print(priciest_pizza)
+
+three_cheapest = pizza_and_prices[0:3]
+
+print("The three cheapest pizzas are : ", three_cheapest)
+
+#print(pizza_and_prices)
